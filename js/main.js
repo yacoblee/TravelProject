30d9df893ad675ca854c596932c94ba884a7fc80
yacoblee <68984086+yacoblee@users.noreply.github.com>
2024-04-26 17:46:01 +0900

    메인 화면 추가 작업
    인기 지역 및 추천숙소 구역 작업
    자바스크립트 추가

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
 
 
-const regions = document.querySelectorAll('.region');
-const nextBtn = document.getElementById('nextBtn');
+// const regions = document.querySelectorAll('.region');
+// const nextBtn = document.getElementById('nextBtn');
 
-let currentIndex = 0;
+// let currentIndex = 0;
 
-nextBtn.addEventListener('click', () => {
-    regions[currentIndex].style.opacity = 0;
+// nextBtn.addEventListener('click', () => {
+//     regions[currentIndex].style.opacity = 0;
 
-    currentIndex = (currentIndex + 1) % regions.length;
+//     currentIndex = (currentIndex + 1) % regions.length;
 
-    regions[currentIndex].style.opacity = 1;
+//     regions[currentIndex].style.opacity = 1;
+// });
+
+// regions[currentIndex].style.opacity = 1;
+// setInterval(() => {
+//     regions[currentIndex].style.opacity = 0;
+//     currentIndex = (currentIndex + 1) % regions.length;
+//     regions[currentIndex].style.opacity = 1;
+// }, 5000);
+
+// for (let i = 4; i < regions.length; i++) {
+//     regions[i].style.opacity = 0;
+//     regions[i].style.display = 'none';
+// }
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const selectAreas = document.querySelector('.selectarea');
+    const tabs = selectAreas.querySelectorAll('li');
+
+    tabs.forEach(tab => {
+        tab.addEventListener('click', function () {
+            tabs.forEach(t => t.classList.remove('selected'));
+
+            tab.classList.add('selected');
+        });
+    });
 });
-
-regions[currentIndex].style.opacity = 1;
-setInterval(() => {
-    regions[currentIndex].style.opacity = 0;
-    currentIndex = (currentIndex + 1) % regions.length;
-    regions[currentIndex].style.opacity = 1;
-}, 5000);
-
-for (let i = 4; i < regions.length; i++) {
-    regions[i].style.opacity = 0;
-    regions[i].style.display = 'none';
-}
